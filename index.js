4155a1c2e1fa70b9c679600932f26d8592453a18
irfan ahmed <irfanahmed99989@gmail.com>
2025-01-26 02:25:25 +0600

    navbar

---
+++ index.js
@@ -0,0 +1,12 @@
+var menu = document.getElementById("menu");
+var nav3 = document.getElementById("nav3");
+var dropdown = document.getElementById("dropdown");
+var dropdown_menu = document.getElementById("dropdown-content");
+
+menu.addEventListener("click", function () {
+    nav3.classList = nav3.classList.contains("hide") ? "nav3 show" : "hide";
+})
+
+dropdown.addEventListener("click", function () {
+    dropdown_menu.classList = dropdown_menu.classList.contains("hide-ul") ? "dropdown-content show-ul" : "hide-ul";
+})
